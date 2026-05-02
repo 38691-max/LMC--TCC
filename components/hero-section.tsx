a96f2be2c03379cfd5721fa8aaa9b431c1a40a8f
38691-max <38691@raphaeldisanto.com.br>
2026-05-02 11:36:42 -0300

    style(Landing Page): :lipstick: 2 img to Dark & White mode

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -6,7 +6,6 @@ import { TextEffect } from "@/components/motion-primitivies/text-effect";
 import { HeroHeader } from "./header";
 import Image from "next/image";
 import { Variants } from 'motion/react';
-import Background from "@/public/background.png";
 import {
   RiNextjsFill,
 } from "react-icons/ri";
@@ -24,6 +23,8 @@ import { FaDocker } from "react-icons/fa6";
 import FooterSection from "./footer";
 import TeamSection from "./team-section";
 import TestimonialsSection from "./testimonials-section";
+import Lifestyle from "@/public/lifestyle.png"
+import VibrantParty from "@/public/vibrant-party.png"
 
 export const transitionVariants: {
   item: Variants;
@@ -238,14 +239,14 @@ export default function HeroSection() {
                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                   <Image
                     className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                    src={Background}
+                    src={Lifestyle}
                     alt="app screen"
                     width={2700}
                     height={1440}
                   />
                   <Image
                     className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                    src={Background}
+                    src={VibrantParty}
                     alt="app screen"
                     width={2700}
                     height={1440}
